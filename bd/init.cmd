739e25b0d370c8334386f2cd1824d340b09b9984
wo35t3y <s_w_rd@yahoo.com>
2016-02-10 19:44:44 +0500

    vsyakoe derbmo
    11121222131232534654

---
+++ bd/init.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -u root --password=123456 < init.sql
+pause
--- bd/init.sql
+++ bd/init.sql
@@ -1,9 +1,13 @@
 -- Скрипт инициализации БД
 
+DROP DATABASE IF EXISTS phpslut;
+CREATE DATABASE phpslut;
+
+USE phpslut;
 
 CREATE TABLE article
 (
-	article_id INT AUTOINCREMENT,
+	article_id INT PRIMARY KEY AUTO_INCREMENT,
 	article_title VARCHAR( 1000 ),
 	article_text VARCHAR( 10000 )
 );
