2f46d61dd9f239aa583e23d4d39f5694681c5008
rurubell <rururubell@gmail.com>
2016-02-12 17:28:37 +0000

    20160212172828

---
+++ bd/init.sh
@@ -0,0 +1,1 @@
+mysql -u root --password=123456 < init.sql
---
+++ php/conf/MySysConst.php
@@ -0,0 +1,11 @@
+<?php
+	#Класс - хранитель контант - параметров
+	class MySysConst
+	{
+		#параметры mysql
+		public static $S_MYSQL_USER = "root";
+		public static $S_MYSQL_PASS = "123456";
+		public static $S_MYSQL_HOST = "localhost";
+		public static $S_MYSQL_DB_NAME = "phpslut";
+	}
+?>
