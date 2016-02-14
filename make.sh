562a08d6a50abb8732031f9e7929570bfb81af7f
rurubell <rururubell@gmail.com>
2016-02-14 20:39:40 +0500

    20160214203939

--- make.sh
+++ make.sh
@@ -1,41 +1,20 @@
 #!/bin/bash
 
 
-#Директория веб-сервера
-web_serv_dir="/var/www/html/";
-
-
-#Копирование нужных файлов в директорию веб-сервера
-function __copyToWeSDir()
+#Проверка всех php файлов на ошибки
+function __checkAllPhpFiles
 {
-	cp -R ./php $web_serv_dir;
-	cp -R ./html $web_serv_dir;
-	cp -R ./css $web_serv_dir;
-	cp -R ./res $web_serv_dir;
-	cp -R ./conf $web_serv_dir;
-	cp -R ./sql $web_serv_dir;
-	
-	chmod 777 -R $web_serv_dir*;
+	find * -type f -iname *.php | while read phpfile; do
+		php -l $phpfile;
+	done
 }
 
 
-#Удаляем все из директории веб-сервера
-function __clearWSDir()
+#main
+function __main
 {
-	rm -rf $web_serv_dir*;
+	__checkAllPhpFiles;
 }
 
 
-#Удаляем сервисные файлы из директории веб-сервера
-function __deleteServiceFilesFromWSDir()
-{
-	cd $web_serv_dir;
-	find -name "*.make" -exec rm -rf {} +;
-	find -name "*.run" -exec rm -rf {} +;
-	find -name "*.commit" -exec rm -rf {} +;
-}
-
-
-__clearWSDir;
-__copyToWeSDir;
-__deleteServiceFilesFromWSDir;
+__main;
---
+++ run.sh
@@ -0,0 +1,1 @@
+php ./php/main.php
