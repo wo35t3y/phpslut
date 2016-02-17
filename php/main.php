7a9d50d76ce76882bcbe5b200f0687f659314fd9
rurubell <rururubell@gmail.com>
2016-02-17 22:19:52 +0500

    20160217221945 Добавил error_handler

--- php/main.php
+++ php/main.php
@@ -1,6 +1,26 @@
 <?php
+	ini_set( "display_errors", 0 );
+	error_reporting( E_ALL );
+	set_error_handler( "myErr" );
+	
 	require_once __DIR__ . "/util/MyLogWriter.php";
 	
-	$mlw = new MyLogWriter();
-	$mlw->writeErr( "hello!" );
+	
+	/* Здесь можно писать код */
+	
+	
+	echo "Hello world!";
+	
+	
+	/* Тут больше нельзя! */
+	
+	
+	
+	#Функция обработчик ошибок
+	function myErr( $errno, $errmsg, $file, $line ) 
+	{
+		$s_errmess = $errno . "\t" . $errmsg . "\t" . $file . "\t" . $line;
+		$mlw = new MyLogWriter();
+		$mlw->writeErr( $s_errmess );
+	}
 ?>
